e91f74c8f2b038f94d1fc750b285b823b7d87143
Nikhil Patil <patilnikhils19@gmail.com>
2016-10-12 21:50:56 -0600

    Designed Argument parser

---
+++ UnstructuredPeer.py
@@ -0,0 +1,33 @@
+import os
+import argparse
+import sys
+import socket
+import socket
+import subprocess
+import time
+import thread
+from thread import *
+import datetime
+import logging
+
+
+if __name__ == '__main__':
+
+          parser = argparse.ArgumentParser()#Define argument parser
+          parser.add_argument("-p","--port",help="Port Number",type=int)
+          parser.add_argument("-b","--bootstrapip",help="Bootstrap Server IP address",type=str)
+          parser.add_argument("-n","--bootstrapport",help="Bootstrap Server Port Number",type=int)
+          parser.add_argument("-u","--username",help = "Username for registering with the bootstrap Server",type=str)
+          args = parser.parse_args()
+
+          if not args.bootstrapip or  not args.port or not args.bootstrapport :
+                 print("Need Bootstrap ServerIP")
+
+          elif not args.port :
+                 print("Need Port Number")
+
+          elif  not args.bootstrapport :
+                 print("Need Bootstrap Server Port Number")
+
+          else :
+
